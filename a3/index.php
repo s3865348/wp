7f40498e07170453b0e967370f17b1f8bd61924e
Alex Bishara <s3865348@student.rmit.edu.au>
2020-08-30 13:32:35 +1000

    Copy of Assignment 2 to Assignment 3
    Copied Previous website for new assignment

---
+++ a3/index.php
@@ -0,0 +1,355 @@
+<!DOCTYPE html>
+<html lang='en'>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="Website dedicated to Douglas Raymond Baker">
+  <meta name="author" content="Alexander John Bishara">
+  <title>ANZAC Douglas Raymond Baker - Letters Home By Alexander John Bishara s3865348</title>
+  <link rel="icon" href="imgs/anzac_logo.jpg" type="image/x-icon"> 
+
+  <!-- Keep wireframe.css for debugging, add your css to style.css -->
+  <link id='wireframecss' type="text/css" rel="stylesheet" href="../wireframe.css" disabled>
+  <link id='stylecss' type="text/css" rel="stylesheet" href="style.css?t=<?= filemtime("style.css"); ?>">
+  <script src='../wireframe.js'></script>
+</head>
+<body>
+  <header name="top" style="text-align:center; ">
+    <img src="imgs/poppy.jpg" alt="Poppy" height="120" />
+    <h3>ANZAC Douglas Raymond Baker - Letters Home</h3>
+  </header>
+
+  <nav>
+    <a href=#Welcome>Welcome</a>
+    <a href=#Intro>Intro</a>
+    <a href=#Letters>Letters and Post Cards</a>
+    <a href=#Descriptions>Descriptions of Battle Actions</a>
+    <a href=#Places>Places</a>
+    <a href=#Links>Links to Related Material</a>
+    <a href=#Comments>Comments</a>
+  </nav>
+
+  <main>
+    <article id=Welcome>
+      <div class="bgimg-1">
+        <div class="caption">
+          <span class="border">Douglas Raymond Baker ANZAC</span>
+        </div>
+      </div>
+        
+      <div class ="section-white">
+          <h3 style="text-align:center;">Home / Welcome</h3>
+          <p> Hello and welcome,
+            
+            This year is the centenary of the birth of the ANZAC legend. As such, many people, particularly young people, are looking for ways to connect with people of that period and inparticular, those who created the ANZAC legend.
+            
+            
+            This site presents the letters of Douglas Raymond Baker, who came from Gympie, Queensland, Australia. He enlisted in August 1914 and during the years that followed, he wrote letters and post cards to his family at home. In them, he describes much of the goings-on of the life of an ANZAC, his feeling and opinions, and experiences while visiting his relatives in England during leave.
+            
+            They start from the beginning of basic training in Brisbane in August 1914 and end in May 1918.
+            
+            They are offered here so that others may get an understanding of life as an ANZAC and get a sense of what life on the battlefield was like.
+            
+          </p>
+      </div>
+    </article>
+
+    <article id=Intro>
+      <div class="bgimg-2">
+        <div class="caption">
+          <span class="border headers">Intro</span>
+        </div>
+      </div>
+
+      <div style="position:relative;">
+        <div class ="section-black" style="text-align: center;">
+          <p>
+            These are copies of letters written by my father Douglas Raymond Baker during the First World War (1914-1918) to his family in Gympie. I have three thick exercise books in which the letters were copied in hand writing by my Aunt Alice, his sister. I understand that this was done so his letters could be sent on to other members of the family. I don’t know if the originals are still in existence, probably not.
+            
+            In the early letters Alice has not included the name of the person they were written to but later she writes at the beginning, “Letter to Dot”,  “Letter to Mother”, etc. and later still copies the original, “Dear Al”, “Dear Mag”, etc..  Likewise the endings are often not laid out fully as we would do but Al may have done this or Dad may have been saving space on the page.
+            
+            Some letters are slightly out of date order in the books but as most of these are around the time his father died I have put them in the correct order as this is crucial to understanding the sense of the contents.  A lot will be missing – many, many ships were sunk but thanks to Aunt Alice we have these copies of the ones that did get through.
+            
+            Where Dad used brackets in a letter I have used [   ]  style and where I have made any comment or explanation I have used (   ) and Italics.
+            
+            Where he uses the word “gay” it is used in the true sense, bright, happy, carefree, etc., This is the original meaning of the word before it became associated with the homosexual community.
+            
+            The amounts of money are, of course, in Pounds, shillings and pence but as these are out of date and we have no pounds sign in the computer I have written them in, sometimes showing the decimal equivalents. One Pound equalled $2,  one shilling equalled 10 cents and one penny equalled a little under one cent. After a while I stopped putting the equivalents in as they had no significance unless you knew the relative cost of things then and now. A shilling – equivalent to ten cents might actually alent to ten cents might actually buy  buy ten or twenty dollars worth of goods now.
+          </p>
+          <table id="t01">
+            <tr>
+              <th>Abbreviations And Terms</th>
+              <th></th> 
+            </tr>
+            <tr>
+              <td>Coy</td>
+              <td>= Company, (Part of a Batallion)</td>
+            </tr>
+            <tr>
+              <td>Batt</td>
+              <td>= Battalion</td>
+            </tr>
+            <tr>
+              <td>Col</td>
+              <td>= Colonel</td>
+            </tr>
+            <tr>
+              <td>Sergt.</td>
+              <td>= Sergeant</td>
+            </tr>
+            <tr>
+              <td>Capt.</td>
+              <td>= Captain</td>
+            </tr>
+            <tr>
+              <td>Lieu</td>
+              <td>=  Lieutenant (pronounced “Lef-tenant”)</td>
+            </tr>
+            <tr>
+              <td>Q.M.</td>
+              <td>= Quarter Master</td>
+            </tr>
+            <tr>
+              <td>Batt</td>
+              <td>= Battalion</td>
+            </tr>
+            <tr>
+              <td>Comp.</td>
+              <td>= Also Company</td>
+            </tr>
+            <tr>
+              <td>PC</td>
+              <td>= Post Card (Not Personal Computer!)</td>
+            </tr>
+            <tr>
+              <td>CB</td>
+              <td>= Confined to Barracks (leave stopped as a punishment)</td>
+            </tr>
+            <tr>
+              <td>Big Hits/ Brass Hats</td>
+              <td>= Top Ranked Officers</td>
+            </tr>
+            <tr>
+              <td>Dry Canteen</td>
+              <td>= bar and/or cafeteria, etc. where  no alcohol is served.</td>
+            <tr>
+              <td>Wet Canteen</td>
+              <td>= bar and/or cafeteria, etc. where alcohol  is available.</td>
+            </tr>
+            <tr>
+              <td>Sapping</td>
+              <td>=  digging a tunnel or deep trench to approach or undermine an enemy position.</td>
+            </tr>
+            <tr>
+              <td>L.Horse</td>
+              <td>= Light Horse</td>
+            </tr>
+            <tr>
+              <td>Good or Great Nick</td>
+              <td>= Good Health</td>
+            </tr>
+            <tr>
+              <td>Bonsor</td>
+              <td>= wonderful, marvellous.     (in 2009 speak,  “awesome”, “cool”.)</td>
+            </tr>
+            <tr>
+              <td>M.G.S.</td>
+              <td>= Machine Gun Section</td>
+            </tr>
+            <tr>
+              <td>Terriers</td>
+              <td>= Territorials</td>
+            </tr>
+            <tr>
+              <td>Territorials</td>
+              <td>=  soldiers of the English  Volunteer Reserve.</td>
+            </tr>
+            <tr>
+              <td>Bomb</td>
+              <td>= unless dropped from an aeroplane these would have been Mills Bombs    –     an early version of the hand grenade.</td>
+            </tr>
+            <tr>
+              <td>Blighty</td>
+              <td>= England</td>
+            </tr>
+            <tr>
+              <td>Duke</td>
+              <td>= Dad’s/family’s dog</td>
+            </tr>
+            <tr>
+              <td>O.R.</td>
+              <td>= Orderly Room</td>
+            </tr>
+          </table>
+          <br>
+          Additional Comments by Ian Stuart Baker
+          (son of Douglas Richard Baker)
+          
+          
+          
+          As the grandson of the Douglas Raymond, I'd like to share some thoughts with modern readers. Apart from the language issues that my Dad highlights, it would also help to recognise that in Grandfathers day, the letter was the only means of international communications for average people (telegrams were expensive and used rarely and international telephone calls, rarer still). That's why these letters are so important and why their content gives us window into their lives.
+          
+          From discussions with my Dad, it has came to light that the content of letters to his direct family deliberately leave out much of the dreadful suffering and drudgery experienced by the diggers. This intentional self-editing was intended to allay the fears and concerns of those at home, inparticular, his mother and sisters.
+          
+          Finally, let me express a debt of thanks to Great Aunt Alice for diligently transcribing the original letters for without this effort, we would not have the material we do today. Also, my gratitude to my father for his work transforming the hand-written script into a typed paper record and then into electronic format, making my part in this infinitely easier.<br></p>
+          <a href="#top">Back to top of page</a>
+        </div>
+      </div>
+    </article>
+
+    <article id=Letters>
+      <div class="bgimg-3">
+        <div class="caption">
+          <span class="border headers">Postcards and Letters</span>
+        </div>
+      </div>
+
+      <div style="position:relative;">
+        <div class ="section-black">
+          Below is an example of the texts used for the letters and postcards as correspondence. Sample used "On the first page of the exercise book Aunt Alice has written:-
+          
+          
+          
+          Book No. I
+          
+          written by Alice Baker.
+          
+          
+          
+          Letters received from D. R. Baker after
+          
+          his enlistment for the war  Sept.  1914.
+          
+          "
+          <p class="handwriting">
+            August 25th 1914.
+            
+            Just have a chance to write a few lines at a small shop near the Camp. We had about four hours in Brisbane before going to Camp and had a good look round. Arrived in Camp about 6 p.m. and all the Gympie lads [Infantry] were placed in one tent [11 of us].  The Light Horse are nearly a mile from us. Of course it was my luck to be made Tent Orderly for the first day – getting the tucker and cleaning up. We have had nothing issued to us yet, so just as well I brought something.  There is no child’s play in this camp – plenty of hard work – they mean business. We have a free hand at night so far and can write as we like.  Saw Herb as we were marching to the Camp. He was going back from work and I had only time to shake hands with him – could not leave the ranks. We will be under Captain Jackson. He has been appointed Captain of the Northern Rivers men and managed to get us in with him. Cannot write any more, am in a hurry. Will write more later on. Am feeling quite homesick. Had our feet examined today, teeth tomorrow I think.
+          </p>
+          <br>
+          <p style="text-align: center;">
+            <a href="#top">Back to top of page</a>
+          </p>
+        </div>
+      </div>
+    </article>
+
+    <article id=Descriptions>
+      <div class="bgimg-4">
+        <div class="caption">
+          <span class="border headers">Descriptions of Battle Action</span>
+        </div>
+      </div>
+                
+      <div style="position:relative;">
+        <div class ="section-black">
+          <p>    Below is an example of the texts used for the letters and postcards as correspondence. Sample used "On the first page of the exercise book Aunt Alice has written:-
+            <br>
+            <br>
+            <p class="handwriting">
+              Post Card. 
+              
+              August 25th 1914.
+              
+              Just have a chance to write a few lines at a small shop near the Camp. We had about four hours in Brisbane before going to Camp and had a good look round. Arrived in Camp about 6 p.m. and all the Gympie lads [Infantry] were placed in one tent [11 of us].  The Light Horse are nearly a mile from us. Of course it was my luck to be made Tent Orderly for the first day – getting the tucker and cleaning up. We have had nothing issued to us yet, so just as well I brought something.  There is no child’s play in this camp – plenty of hard work – they mean business. We have a free hand at night so far and can write as we like.  Saw Herb as we were marching to the Camp. He was going back from work and I had only time to shake hands with him – could not leave the ranks. We will be under Captain Jackson. He has been appointed Captain of the Northern Rivers men and managed to get us in with him. Cannot write any more, am in a hurry. Will write more later on. Am feeling quite homesick. Had our feet examined today, teeth tomorrow I think.
+              
+              
+              
+              
+              
+              ---oooOooo---
+            </p>
+            <p style="text-align: center;">
+              <a href="#top">Back to top of page</a>
+            </p>
+          </p>
+        </div>
+      </div>
+    </article>
+
+    <article id=Places>
+      <div class="bgimg-5">
+        <div class="caption">
+          <span class="border headers">Places</span>
+        </div>
+      </div>
+        
+      <div class ="section-white" style="text-align: center;">
+        <h3 style="text-align:center;">Google Maps Tour</h3>
+        <p>Please click on the link below to view a tour I have compiled highlighting the journey that Douglas has travelled on!
+          <br>
+          <a href="https://tinyurl.com/DouglasBakerANZAC"> Click here for the tour</a>
+          <br>
+          <a href="#top">Back to top of page</a>
+        </p>
+      </div>
+    </article>
+
+    <article id=Links>
+      <div class="bgimg-2">
+        <div class="caption">
+          <span class="border headers">Links</span>
+        </div>
+      </div>
+
+      <div style="position:relative;">
+        <div class ="section-black" style="text-align: center;">
+          <p>External Links relating to the ANZAC story</p>
+          <p>
+            <a href="http://recordsearch.naa.gov.au/scripts/Imagine.asp?B=3009496" rel="nofollow">His complete original service record (Note: this is 20 pages long.)</a>
+            <br>
+            <a href="https://www.aif.adfa.edu.au/showPerson?pid=11163" rel="nofollow">Douglas Raymond BAKERs summary of service record in the AIF Project</a>
+            <br>
+            <a href="https://www.google.com.au/search?hl=en&amp;site=imghp&amp;tbm=isch&amp;source=hp&amp;biw=1920&amp;bih=982&amp;q=Omrah&amp;oq=Omrah&amp;gs_l=img.12...5422.5422.0.6592.1.1.0.0.0.0.212.212.2-1.1.0.msedr...0...1ac.1.62.img..1.0.0.xuc9Jh0uuzM#imgdii=_&amp;imgrc=_XfTmb11-JZqDM%253A%3BrsnzwSFIEHttOM%3Bhttp%253A%252F%252Fwww.nationalanzaccentre.com.au%252Fsites%252Fdefault%252Ffiles%252Fstyles%252Fcharacter_bite_images%252Fpublic%252Fships%252FH02223--2-.jpg%253Fitok%253Dp6xYoFZI%3Bhttp%253A%252F%252Fwww.nationalanzaccentre.com.au%252Fstory%252Frobert-george-hamilton%3B1363%3B1000">Picture of the 9th Battalion, AIF, boarding the Omrah, Brisbane,</a>
+            <br>
+            <a href="https://www.awm.gov.au/images/collection/items/ACCNUM_LARGE/RCDIG1067548/RCDIG1067548--337-.JPG" target="_blank" rel="nofollow">Embarkation Roll - Australian War Memorial</a>
+            <br>
+            <a href="https://trove.nla.gov.au/newspaper/page/25024780" target="_blank" rel="nofollow">Newspaper Clippings from 1919</a>
+            <br>
+            <a href="  https://www.thegazette.co.uk/London/issue/30188/supplement/7275/data.htm" target="_blank" rel="nofollow">British Newspaper Clippings from 1917</a>
+            <br>
+            <a href="#top">Back to top of page</a>
+          </p>
+        </div>
+      </div>
+    </article>
+
+    <article id=Comments>
+      <div class ="section-white" style="text-align: center;">
+        <h3 style="text-align:center;">Comments</h3>
+        Please send through any suggestions that you have, and they will be forwarded to Ian Baker, Grandson of Douglas.
+        <form action="mailto:ibak6837@bigpond.net.au" method="post" enctype="text/plain">
+          <p style="color:blue">Name:</p>
+          <br>
+          <input type="text" name="name">
+          <br><br>
+          <label for="email">Email:</label>
+          <br>
+          <input type="email" id="email" name="email">
+          <br><br>
+          Comment:
+          <br>
+          <input type="textarea" name="comment">
+          <br><br>
+          <input type="submit" value="Send">
+          <input type="reset" value="Reset">
+        </form>
+        <a href="#top">Back to top of page</a> 
+      </div>
+    </article>
+  </main>
+
+  <footer style="text-align: center;">
+    <div> Content copyright Ian Baker 
+    <br> 
+    Email : ibak6837@bigpond.net.au 
+    </div>
+    <div>&copy;<script>document.write(new Date().getFullYear());</script> Alexander John Bishara, s3865348
+    <br> 
+    <a href="https://github.com/s3865348/wp"> Alexander John Bishara Github Repository</a>
+        Last modified <?= date ("Y F d  H:i", filemtime($_SERVER['SCRIPT_FILENAME'])); ?>.</div>
+    <div>Disclaimer: This website is not a real website and is being developed as part of a School of Science Web Programming course at RMIT University in Melbourne, Australia.</div>
+    <div><button id='toggleWireframeCSS' onclick='toggleWireframe()'>Toggle Wireframe CSS</button></div>
+  </footer>
+</body>
+</html>
